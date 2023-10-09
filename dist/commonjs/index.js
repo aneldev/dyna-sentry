d27eb2d0824aa86e50acd01968c8f22cedf1b184
dennisat <dennis@anel.co>
2023-10-09 11:55:36 +0200

    Upgrade to Sentry v7 & boilerplate

--- dist/commonjs/index.js
+++ dist/commonjs/index.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.EConsoleType = exports.ELevel = exports.DynaSentry = void 0;
 var DynaSentry_1 = require("./DynaSentry");
-exports.DynaSentry = DynaSentry_1.DynaSentry;
-exports.ELevel = DynaSentry_1.ELevel;
-exports.EConsoleType = DynaSentry_1.EConsoleType;
+Object.defineProperty(exports, "DynaSentry", { enumerable: true, get: function () { return DynaSentry_1.DynaSentry; } });
+Object.defineProperty(exports, "ELevel", { enumerable: true, get: function () { return DynaSentry_1.ELevel; } });
+Object.defineProperty(exports, "EConsoleType", { enumerable: true, get: function () { return DynaSentry_1.EConsoleType; } });
 //# sourceMappingURL=index.js.map
